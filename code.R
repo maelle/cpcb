64c0b02c5320ed74aa08c655ce84d051f42e8a0e
Maëlle Salmon <maelle.salmon@yahoo.se>
2016-09-15 20:19:47 +0200

    adds code for wrangling the table

--- code.R
+++ code.R
@@ -1,4 +1,10 @@
 library('RSelenium')
+library("tidyr")
+library("dplyr")
+library("lubridate")
+library("tibble")
+library("lazyeval")
+
 RSelenium::startServer(args = c("-Dwebdriver.chrome.driver=C:/Users/Maelle/Documents/cpcb/chromedriver.exe")
                        , log = FALSE, invisible = FALSE)
 remDr <- remoteDriver(browserName = "chrome")
@@ -46,3 +52,27 @@ Sys.sleep(1)
 webElem <- remDr$findElement(using = 'id', value = "btnSubmit")
 webElem$clickElement()
 Sys.sleep(1)
+
+# get table and wrangle it
+webElem <- remDr$findElement(using = 'id', value = "gvReportStation")
+table <- webElem$getElementText()
+table <- strsplit(table[[1]], "\n")[[1]]
+table <- tibble_(list(content = ~table[4:length(table)]))
+table <- separate_(table, "content", sep = "  ",
+                  into = c("parameter", "start_time", "end_time",
+                           "date", "unit"))
+table <- separate_(table, "date", sep = " ",
+                   into = c("date", "concentration"))
+table <- select_(table, quote(- parameter))
+table <- mutate_(table, row = interp(~1:nrow(table)))
+table <- group_by_(table, "row")
+table <- mutate_(table, start = interp(~dmy_hms(paste(date, start_time))))
+table <- mutate_(table, start = interp(~ force_tz(start, tzone = "Asia/Kolkata")))
+table <- mutate_(table, end = interp(~dmy_hms(paste(date, end_time))))
+table <- mutate_(table, end = interp(~ force_tz(start, tzone = "Asia/Kolkata")))
+table <- select_(table, quote(- date))
+table <- select_(table, quote(- start_time))
+table <- select_(table, quote(- end_time))
+table <- ungroup(table)
+table <- select_(table, quote(- row))
+table <- mutate_(table, unit = interp(~gsub(" NA NA", "", unit)))
